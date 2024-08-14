1b27bfecb9df5b0dc5f2601fd28501303af85647
Faith-Watiri <watiri216@gmail.com>
2024-08-13 19:28:40 -0700

    commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python3
+"""
+Function that queries  the Reddit API and returns
+the number of subscribers for a given subreddit
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = requests.get("https://www.reddit.com/r/{}/about.json")
+                    .format(subreddit), headers={"User-Agent": "Imaginary_Big_8292"}
+    if url.status_code == 200:
+        return url.json().get("data").get("subscribers")
+    else:
+        return 0
