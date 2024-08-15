779f56d122b739107b6fb6898ea7363ece01ed53
Faith-Watiri <watiri216@gmail.com>
2024-08-15 07:42:12 -0700

    commit 100

---
+++ 0x16-api_advanced/100-county.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""
+parses title of all hot articles, and prints a sorted count
+of given keywords (case-insensitive, delimited by spaces.
+Javascript should count as javascript, but java should not).
+"""
+
+import pprint
+import requests
+        
+BASE_URL = 'http://reddit.com/r/{}/hot.json'
+
+
+def count_words(subreddit, word_list, hot_list=[], after=None);
+ '''function count_words : Get All hot posts'''
+    headers = {'User-agent':'Unix:0-subs:v1'}
+    params = {'limit': 100}
+    if isnstance(after, str):
+        if after != "STOP":
+            params['after']=after
+
+        else:
+            return print_results(word_list, hot_list)
+        response = requests.get(BASE_URL.forma(subreddit),
+                               headers=headerd, parms=params)
+                               
+        if respons.status _code != 200:
+            return None
+    data = response.json().get('data', {})
+    after = data.get('after', 'STOP')
+    if not after:
+        after = "STOP"
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+    return count_words(subreddit, word_list, hot_list, after)
+
+
+def print_results(word_list, hot_list):
+    '''function print_results :Prints request results'''
+    count = {}
+    for word in word_list:
+        count[word] = 0
+    for title in hot_list:
+        for word in word_list:
+            count[word] = count[word] +\
+             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
+
+             count = {k: v for k, v in count.items() if v > 0}
+    words = sorted(list(count.keys()))
+    for word in sorted(words,
+                       reverse=True, key=lambda k: count[k]):
+        print("{}: {}".format(word, count[word]))
+
