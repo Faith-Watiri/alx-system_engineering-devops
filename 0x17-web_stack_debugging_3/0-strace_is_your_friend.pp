ae647252c8942cb11174a340007cad17bcdee1e1
Faith-Watiri <watiri216@gmail.com>
2024-08-18 11:25:00 -0700

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# incorrect file name
+#incorrect file name
 exec{'fix-wordpress':
      command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/ww/html/wp-settings.php',
         path => '/usr/local/bin/:/bin/',
